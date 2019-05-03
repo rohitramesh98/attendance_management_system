9b51f4dacdd383b21a53d5d819a89764d23810a5
rohitramesh98 <rohitramesh98@gmail.com>
2019-05-03 14:13:12 +0530

    admin login

--- adminlogin.php
+++ adminlogin.php
@@ -1,3 +1,40 @@
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="bootstrap.css">
+       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <style>
+        #bc{
+              height: 43px;
+              width: 480px;
+              padding: 20px;
+            }
+        .a12{
+              padding: 10px;
+                margin: 8% auto 50%;
+                display: table;
+                height: 250px;
+                /*position: relative;*/
+                overflow: hidden;
+                width: 700px;
+                background-image:url("form.jpg");
+                background-repeat: repeat-x;
+                background-size: contain;
+                
+            }
+            input[type=submit]
+            {
+                padding:10px 215px;
+                font-weight: bold;
+                background-color: 00ffff;
+                text-transform: uppercase;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
 <?php  include("connection.php"); ?>
 
 <?php
@@ -21,7 +58,28 @@ if(isset($_POST['login']))
     }  
     else  
     {  
-      echo "<script>alert('username or password is incorrect!')</script>";  
+      echo '<div class="alert alert-danger alert-dismissible fade show" style="text-align:center;">
+    <button type="button" class="close" data-dismiss="alert">&times;</button>
+    <strong>Error!</strong> Username or password is invalid!  </div>';
     }  
 }  
 ?>  
+        <div align="center" class="a12">
+       <h2><p><font color="white">ADMIN DETAILS</font></p></h2>
+        <br>
+        <form action="adminlogin.php" method="post">
+            <div align="left" style="float: left"><br>
+              <font color="white">
+                Username* <br><br><br>  
+                Password* <br><br><br>  
+              </font>
+            </div>
+            <div align="left" style="float: right">
+            <input type="text" placeholder="username"  id="bc"  required name="username"><br><br>
+            <input type="password" placeholder="password"  id="bc"  required name="password"><br><br>
+            <input type="submit" name="login" value="LOGIN">
+            </div>
+        </form>
+        </div>
+    </body>    
+</html>
